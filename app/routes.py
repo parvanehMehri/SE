37937ccc0ecbaadfc18df1ba4cf94c51653ebfdf
parvanehMehri <parvaneh.m1376@gmail.com>
2020-02-09 00:39:33 +0330

    L_R

--- app/routes.py
+++ app/routes.py
@@ -112,5 +112,5 @@ def goals():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+    return send_from_directory(os.path.join(app.root_path, '../static'),
                                'favicon.ico', mimetype='img/vnd.microsoft.icon')
